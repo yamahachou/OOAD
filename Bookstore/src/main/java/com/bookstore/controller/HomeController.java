9314c095ef860d252d57e1b161463f81279f054a
yamahachou <yamahachou@hotmail.com>
2018-11-01 00:30:32 -0600

    First code commit

---
+++ Bookstore/src/main/java/com/bookstore/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+	
+	@RequestMapping("/MyAccount")
+	public String MyAccount(){
+		return "MyAccount";
+	}
+	
+	
+
+}
---
+++ Bookstore/src/main/java/com/bookstore/domain/User.java
@@ -0,0 +1,43 @@
+package com.bookstore.domain;
+
+public class User extends Account{
+	
+
+	private Long id;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public boolean isEnabled() {
+		return enabled;
+	}
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	private String firstname;
+	private String lastname;
+	private String phone;
+	private boolean enabled=true;
+	
+
+}
